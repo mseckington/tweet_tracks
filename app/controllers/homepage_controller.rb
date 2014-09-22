0e2691fddb37605ddb238add45aef45753c2e558
Melinda Seckington <mseckington@gmail.com>
2014-09-22 11:16:35 +0100

    Add search page

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -7,6 +7,76 @@ class HomepageController < ApplicationController
     @tweets = Tweet.all.order('created_at DESC').limit(10)
   end
 
+  def search
+    @searchword = params[:user]
+
+    if @searchword.present?
+
+      client = Twitter::REST::Client.new do |config|
+        config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
+        config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+        config.access_token = ENV['TWITTER_ACCESS_TOKEN']
+        config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+      end
+
+      tweets = client.user_timeline(@searchword, count: 5)
+
+      conn = Faraday.new(:url => 'https://api.dandelion.eu') do |faraday|
+        faraday.request  :url_encoded             # form-encode POST params
+        faraday.response :logger                  # log requests to STDOUT
+        faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+      end
+
+      @tweets = []
+
+      tweets.each do |tweet|
+        response = conn.get "/datatxt/nex/v1/?min_confidence=0.5&lang=en &social.parse_hashtag=True&text=#{tweet.text}&include=image&$app_id=#{ENV['DANDELION_APP_ID']}&$app_key=#{ENV['DANDELION_APP_KEY']}"
+        response = conn.get "/datatxt/nex/v1/?min_confidence=0.5&lang=en &social.parse_hashtag=True&text=#{tweet.text}&include=image&$app_id=cb93dde4&$app_key=5366ca18b641266f05c0447a0ee8b1a8"
+        converted_response = JSON.parse(response.body)
+
+        next if converted_response["annotations"].blank?
+
+        # image_urls = converted_response["annotations"].inject([]){|images, x| x["image"]["thumbnail"] ? images << x["image"]["thumbnail"] : images}
+        # puts image_urls
+
+        entities = converted_response["annotations"]
+
+        track = "emptyempty"
+        label = ""
+
+        entities.each do |entity|
+          label = entity["label"]
+          tracks = RSpotify::Track.search(label)
+          puts tracks.count
+
+          if tracks.count > 0
+            track = tracks.first
+            break
+          end
+        end
+
+        puts track
+        puts label
+        next if track == "emptyempty"
+
+        puts "herehere"
+        puts label
+
+        @tweets << {
+          text: tweet.text,
+          label: label,
+          track_name: track.name,
+          track_image: track.album.images[1]["url"].to_s,
+          track_url: track.external_urls["spotify"],
+          track_artist: track.artists.map{|a| a.name}.join(" & "),
+          spotify_id: track.id,
+          user_name: tweet.user.screen_name,
+          user_image: tweet.user.profile_image_url.to_s
+        }
+      end
+    end
+  end
+
   def reload_tweets
     client = Twitter::REST::Client.new do |config|
       config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
@@ -27,6 +97,7 @@ class HomepageController < ApplicationController
 
     tweets.each do |tweet|
       response = conn.get "/datatxt/nex/v1/?min_confidence=0.5&lang=en &social.parse_hashtag=True&text=#{tweet.text}&include=image&$app_id=#{ENV['DANDELION_APP_ID']}&$app_key=#{ENV['DANDELION_APP_KEY']}"
+      response = conn.get "/datatxt/nex/v1/?min_confidence=0.5&lang=en &social.parse_hashtag=True&text=#{tweet.text}&include=image&$app_id=cb93dde4&$app_key=5366ca18b641266f05c0447a0ee8b1a8"
       converted_response = JSON.parse(response.body)
 
       next if converted_response["annotations"].blank?
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   get '/reload', to: 'homepage#reload_tweets'
 
+  get '/search', to: 'homepage#search'
+  post '/search', to: 'homepage#search', as: 'new_search'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
